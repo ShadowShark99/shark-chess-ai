c4e28de4e49a76e6491cdfffe10ee454007d4a91
ShadowShark99 <kadenwchan@gmail.com>
2025-05-27 19:13:41 -0500

    Develop macro to generalize directional rays

--- src/rayattacks.rs
+++ src/rayattacks.rs
@@ -2,38 +2,73 @@ type Bitboard = u64;
 
 
 pub struct Rays{
-  n_rays: Vec<u64>,
+  n_rays: Vec<Bitboard>,
+  e_rays: Vec<Bitboard>,
+  s_rays: Vec<Bitboard>,
+  w_rays: Vec<Bitboard>,
+}
+
+macro_rules! make_rays{
+  ($ray_fn:ident) => {{
+    
+    let mut rays = vec![];
+    for row in 1..=8{
+      for col in 1..=8{
+        rays.push($ray_fn(row,col));
+      }
+    }
+    rays
+  }};
 }
 
 impl Rays{
   fn initialize() -> Self {
-    let mut n_rays = vec![];
+    let n_rays = make_rays!(n_ray);
+    let e_rays = make_rays!(e_ray);
+    let s_rays = make_rays!(s_ray);
+    let w_rays = make_rays!(w_ray);
     
-    for row in 1..=8{
-      for col in 1..=8{
-        n_rays.push(n_ray(row,col));
-      }
-    }
 
     Self{
       n_rays,
+      e_rays,
+      s_rays,
+      w_rays,
     }
   }
 }
 
-fn n_ray(row: u64, col: u64) -> Bitboard{
-  let mut bitboard = 0;
 
-  for offset in 1..=8{
-    if row + offset > 8 {
-      break;
+//set bits for directional rays: n, e, s, w
+macro_rules! define_ray{
+  ($name:ident, $offset_fn:expr) => {
+    fn $name(row: i64, col: i64) -> Bitboard{
+      let mut bitboard = 0;
+    
+      for offset in 1..=8{
+        bitboard = set_bit(bitboard, $offset_fn(row, col, offset));
+      }
+      bitboard
     }
-    bitboard = set_bit(bitboard, row + offset, col);
   }
-  bitboard
+
 }
 
-fn set_bit(bitboard: Bitboard, row: u64, col: u64) -> Bitboard{
+//each direction function is defined for n, e, s, w
+define_ray!(n_ray, |row, col, offset| (row + offset, col));
+define_ray!(e_ray, |row, col, offset| (row, col + offset));
+define_ray!(s_ray, |row, col, offset| (row - offset, col));
+define_ray!(w_ray, |row, col, offset| (row, col - offset));
+
+fn set_bit(bitboard: Bitboard, row_col: (i64, i64)) -> Bitboard{
+  let row = row_col.0;
+  let col = row_col.1;
+  //return board if out of bounds
+  if(row < 1 || row > 8 || col < 1 || col > 8)
+  {
+    return bitboard;
+  }
+  //or with respective board coordinate, to set that square on the board
   bitboard | (1 << ((col - 1) + (row - 1) * 8))
 }
 
@@ -64,7 +99,6 @@ fn print_bitboard(bitboard: Bitboard, mark: Option<usize>) -> String{
     if((i+1) % 8) == 0 {
       row.push_str("\n");
       board.insert_str(0, &row);
-      println!("Here's the bitboard:\n--------\n{}\n---------", board);
       row.clear();
     }
   }
@@ -78,5 +112,8 @@ mod tests{
   #[test]
   fn make_n_ray(){
     println!("Here's the bitboard:\n--------\n{}\n---------", print_bitboard(n_ray(4,4), None));
+    println!("Here's the bitboard:\n--------\n{}\n---------", print_bitboard(e_ray(4,4), None));
+    println!("Here's the bitboard:\n--------\n{}\n---------", print_bitboard(s_ray(4,4), None));
+    println!("Here's the bitboard:\n--------\n{}\n---------", print_bitboard(w_ray(4,4), None));
   }
 }
